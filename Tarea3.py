f032dae2f70e10f84784186d4303ceb8c03e5020
Aless030 <130810466+Aless030@users.noreply.github.com>
2023-09-19 19:16:24 -0400

    Primer reto python

---
+++ Tarea3.py
@@ -0,0 +1,8 @@
+# TODO Solicitar al usuario que ingrese la temperatura  Celsius
+celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+
+# TODO Convertir de Celsius a Fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+
+print(f"{celsius} grados Celsius son equivalentes a {fahrenheit} grados Fahrenheit")
